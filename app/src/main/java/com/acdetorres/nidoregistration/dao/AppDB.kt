cb56adaeffaf6d5a0d4a47fa557a189d60e8fed6
Adrian Clark <42565314+acadetorres@users.noreply.github.com>
2024-07-15 05:25:27 +0800

    -Updated room version for new fields

--- app/src/main/java/com/acdetorres/nidoregistration/dao/AppDB.kt
+++ app/src/main/java/com/acdetorres/nidoregistration/dao/AppDB.kt
@@ -7,7 +7,7 @@ import com.acdetorres.nidoregistration.dao.model.Form
 import com.acdetorres.nidoregistration.dao.model.GetProvincesResponse
 import com.acdetorres.nidoregistration.dao.model.LoggedOnAmbassador
 
-@Database(version = 18, entities = [Form::class, Ambassador::class, LoggedOnAmbassador::class, GetProvincesResponse.Province::class])
+@Database(version = 19  , entities = [Form::class, Ambassador::class, LoggedOnAmbassador::class, GetProvincesResponse.Province::class])
 abstract class AppDB : RoomDatabase() {
 
     abstract fun getRoomDao() : RoomDao
